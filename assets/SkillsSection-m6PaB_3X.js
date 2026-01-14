8b57b464d50a9cc7f9d0d6306cfc3e2dee939f7f
Abdelrahman Saed <me@bnsaed.com>
2026-01-15 00:49:16 +0200

    Add sitemap.xml and update robots.txt; introduce new components for skills and about sections

--- assets/SkillsSection-m6PaB_3X.js
+++ assets/SkillsSection-m6PaB_3X.js
@@ -1,4 +1,4 @@
-import{c as r,j as e}from"./index-BL0snNBR.js";import{u as c}from"./useInView-CadQfsny.js";/**
+import{c as r,j as e}from"./index-DxJWOKeZ.js";import{u as c}from"./useInView-Dw2dgQtJ.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
